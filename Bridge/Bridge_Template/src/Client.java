cfb2d9c44cfe9c4dc31d1be04f0d7f8a325d2c6e
Alieren <ali.eren312@hotmail.com>
2023-09-04 22:00:58 +0300

    update

--- Bridge/Bridge_Template/src/Client.java
+++ Bridge/Bridge_Template/src/Client.java
@@ -4,6 +4,7 @@ public class Client {
         ConcreteImplementation1 ci1 = new ConcreteImplementation1();
         Abstraction abstraction = new Abstraction(ci1);
         abstraction.feature1();
+        abstraction.feature2();
 
         ConcreteImplementation2 ci2 = new ConcreteImplementation2();
         RefinedAbstraction refinedAbstraction = new RefinedAbstraction(ci2);
