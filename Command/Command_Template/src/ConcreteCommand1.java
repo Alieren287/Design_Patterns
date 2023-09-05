48996bcca68e9625444798762c2d8be29c365b44
Alieren <ali.eren312@hotmail.com>
2023-09-05 21:23:10 +0300

    update

--- Command/Command_Template/src/ConcreteCommand1.java
+++ Command/Command_Template/src/ConcreteCommand1.java
@@ -1,4 +1,12 @@
 public class ConcreteCommand1 implements Command {
+    String receiver;
+    String params;
+
+    ConcreteCommand1(String receiver, String params) {
+        this.receiver = receiver;
+        this.params = params;
+    }
+
     @Override
     public void execute() {
         //Command logic
